6e4b42605ed4c4ee1156f02daae10fd5e2cac9fc
Lee Iverson <leei@sociologi.ca>
2017-09-22 17:11:04 -0700

    Use application/json for drag/drop to make input fields off-limits.

--- draganddrop.js
+++ draganddrop.js
@@ -164,7 +164,7 @@
                     var transferDataObject = {data: dragData, channel: sendChannel, offset: offset};
                     var transferDataText = angular.toJson(transferDataObject);
 
-                    e.dataTransfer.setData('text', transferDataText);
+                    e.dataTransfer.setData('application/json', transferDataText);
                     e.dataTransfer.effectAllowed = 'copyMove';
                     
                     
@@ -296,7 +296,7 @@
                     e.stopPropagation(); // Necessary. Allows us to drop.
                 }
 
-                var sendData = e.dataTransfer.getData('text');
+                var sendData = e.dataTransfer.getData('application/json');
                 sendData = angular.fromJson(sendData);
 
                 var dropOffset = calculateDropOffset(e);
